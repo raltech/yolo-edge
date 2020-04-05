7dd3474a27755627c8d72b522576763440adf542
raltech <raltech.dev@gmail.com>
2020-04-05 18:54:23 -0400

    fix yolov3.py decode()

--- core/yolov3.py
+++ core/yolov3.py
@@ -84,12 +84,17 @@ def decode(conv_output, i=0):
     output_size      = conv_shape[1] # i.e., 52 for small, 26 for medium, 13 for large
 
     # Let's use conv_out as 4-d as it is 
-    conv_output = tf.reshape(conv_output, (batch_size, output_size, output_size, 3, 5 + NUM_CLASS))
+    # conv_output = tf.reshape(conv_output, (batch_size, output_size, output_size, 3, 5 + NUM_CLASS))
 
-    conv_raw_dxdy = conv_output[:, :, :, :, 0:2]
-    conv_raw_dwdh = conv_output[:, :, :, :, 2:4]
-    conv_raw_conf = conv_output[:, :, :, :, 4:5]
-    conv_raw_prob = conv_output[:, :, :, :, 5: ]
+
+    # conv_raw_dxdy = conv_output[:, :, :, :, 0:2]
+    conv_raw_dxdy = tf.concat((conv_output[:,:,:,0:2], conv_output[:,:,:,85:87], conv_output[:,:,:,170:172]), -1)
+    # conv_raw_dwdh = conv_output[:, :, :, :, 2:4]
+    conv_raw_dwdh = tf.concat((conv_output[:,:,:,2:4], conv_output[:,:,:,87:89], conv_output[:,:,:,172:174]), -1)
+    # conv_raw_conf = conv_output[:, :, :, :, 4:5]
+    conv_raw_conf = tf.concat((conv_output[:,:,:,4:5], conv_output[:,:,:,89:90], conv_output[:,:,:,174:175]), -1)
+    # conv_raw_prob = conv_output[:, :, :, :, 5: ]
+    conv_raw_prob = tf.concat((conv_output[:,:,:,5:85], conv_output[:,:,:,90:170], conv_output[:,:,:,175:255]), -1)
 
     y = tf.tile(tf.range(output_size, dtype=tf.int32)[:, tf.newaxis], [1, output_size]) # 52x52, 26x26, 13x13
     '''
@@ -127,13 +132,16 @@ def decode(conv_output, i=0):
     '''
 
     xy_grid = tf.concat([x[:, :, tf.newaxis], y[:, :, tf.newaxis]], axis=-1) # simply stack them on top
-    xy_grid = tf.tile(xy_grid[tf.newaxis, :, :, tf.newaxis, :], [batch_size, 1, 1, 3, 1])
+    # => 13x13x2
+    # xy_grid = tf.tile(xy_grid[tf.newaxis, :, :, tf.newaxis, :], [batch_size, 1, 1, 3, 1])
+    xy_grid = tf.tile(xy_grid[tf.newaxis, :, :, :], [batch_size, 1, 1, 3]) # => 4x13x13x6
     xy_grid = tf.cast(xy_grid, tf.float32)
-    # => TensorShape([batch_size(=4), 13, 13, 3, 2])
+    # => TensorShape([batch_size(=4), 13, 13, 3, 2]) (original)
+    # => 4,13,13,6 (modified)
 
     # Scale as described in the original paper
     pred_xy = (tf.sigmoid(conv_raw_dxdy) + xy_grid) * STRIDES[i]
-    pred_wh = (tf.exp(conv_raw_dwdh) * ANCHORS[i]) * STRIDES[i]
+    pred_wh = (tf.exp(conv_raw_dwdh) * tf.reshape(ANCHORS[i], [-1])) * STRIDES[i]
     pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
     '''
     print('decode')
@@ -142,6 +150,9 @@ def decode(conv_output, i=0):
     print(pred_xywh.shape) => (None, None, None, 3, 4)
     so, last 4 for x,y,w,h
     '''
+
+    # print('decode')
+    # print(pred_xywh.shape) => (None, 52, 52, 12)
 
     pred_conf = tf.sigmoid(conv_raw_conf)
     pred_prob = tf.sigmoid(conv_raw_prob)
